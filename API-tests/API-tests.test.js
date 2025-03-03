1f7f79f11183f4ac16f9f3e04c892107be17948e
letoproject <dmitry.sytnyk@gmail.com>
2025-03-03 15:32:35 +0200

    Added serialization and deserialization to POST test

--- API-tests/API-tests.test.js
+++ API-tests/API-tests.test.js
@@ -1,6 +1,24 @@
 const axios = require("axios");
 
+class BlogPost {
+  constructor(userId, title, body) {
+    this.userId = userId;
+    this.title = title;
+    this.body = body;
+  }
+
+  // Method for deserialization
+  static fromApiResponse(apiResponse) {
+    return new BlogPost(
+      apiResponse.userId,
+      apiResponse.title,
+      apiResponse.body
+    );
+  }
+}
+
 describe("JSONPlaceholder API Tests", () => {
+  // GET
   test("GET /users/1 returns correct data", async () => {
     // Make the HTTP GET request
     const response = await axios.get(
@@ -23,6 +41,7 @@ describe("JSONPlaceholder API Tests", () => {
     expect(response.data.company.bs).toBe("harness real-time e-markets");
   });
 
+  // GET + parameterizing
   test.each([
     [1, "Leanne Graham", "harness real-time e-markets"],
     [2, "Ervin Howell", "synergize scalable supply-chains"],
@@ -42,6 +61,7 @@ describe("JSONPlaceholder API Tests", () => {
     }
   );
 
+  // POST
   test("POST Submit a new blog post", async () => {
     const requestBody = {
       userId: 1,
@@ -61,5 +81,34 @@ describe("JSONPlaceholder API Tests", () => {
     expect(response.status).toBe(201);
     expect(response.data).toHaveProperty("id");
     expect(typeof response.data.id).toBe("number");
+  });
+
+  // POST + serializing
+  test("POST Submit a new blog post(serialized and deserialized)", async () => {
+    const newPost = new BlogPost(
+      1,
+      "My blog post title",
+      "This is the text of my latest blog post."
+    );
+
+    const headers = {
+      "Content-type": "application/json",
+    };
+
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      newPost,
+      { headers }
+    );
+    expect(response.status).toBe(201);
+    expect(response.data).toHaveProperty("id");
+    expect(typeof response.data.id).toBe("number");
+
+    // Deserializing
+    const deserializedPost = BlogPost.fromApiResponse(response.data);
+
+    expect(deserializedPost.userId).toBe(newPost.userId);
+    expect(deserializedPost.title).toBe(newPost.title);
+    expect(deserializedPost.body).toBe(newPost.body);
   });
 });
